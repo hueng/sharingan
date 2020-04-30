cdc22eeb492f2a6c49ed4e2b900d612e5d62f33c
yufeng <yufeng@didichuxing.com>
2020-04-30 12:16:09 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-echo "=====install go1.13====="
-curl -s https://raw.githubusercontent.com/didi/sharingan-go/recorder/install/go1.13 | sh  > /dev/null 2>&1
-export GOROOT=/tmp/recorder-go1.13
-export PATH=$GOROOT/bin:$PATH
+workspace=$(cd $(dirname $0) && pwd -P)
+info_msg="\033[;32m[INFO]\033[0m\t"
 
-echo "=====build example====="
-cd example
-go build -tags="recorder"
-if [ $? -ne 0 ]; then
-   exit 1
-fi
+function build(){
+    echo "${info_msg}-> install go1.13"
+    curl -s https://raw.githubusercontent.com/didi/sharingan-go/recorder/install/go1.13 | sh  > /de
+    export GOROOT=/tmp/recorder-go1.13
+    export PATH=$GOROOT/bin:$PATH
 
-go build -tags="replayer" -gcflags="all=-N -l"
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-cd -
+    cd ${workspace}/example
+    echo "${info_msg}-> build recorder"
+    go build -tags="recorder"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 
-echo "=====build recorder-agent====="
-cd recorder-agent
-go build
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-cd -
+    cd ${workspace}/example
+    echo "${info_msg}-> build replayer"
+    go build -tags="replayer" -gcflags="all=-N -l"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
 
-echo "=====build replayer-agent====="
-cd replayer-agent
-go build
-if [ $? -ne 0 ]; then
-   exit 1
-fi
-cd -
+    cd ${workspace}/recorder-agent
+    echo "${info_msg}-> build recorder-agent"
+    go build
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+
+    cd ${workspace}/replayer-agent
+    echo "${info_msg}-> build replayer-agent"
+    go build
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+build
+
