b6b528d07a25ba3e223b41d6c7c4627f4896c3ef
yufeng <yufeng@didichuxing.com>
2020-04-30 11:23:02 +0800

    test ci

--- build.sh
+++ build.sh
@@ -6,13 +6,26 @@ export PATH=$GOROOT/bin:$PATH
 
 cd example
 go build -tags="recorder"
+if [ $? -ne 0 ]; then
+   exit 1
+fi
+
 go build -tags="replayer" -gcflags="all=-N -l"
+if [ $? -ne 0 ]; then
+   exit 1
+fi
 cd -
 
 cd recorder-agent
 go build
+if [ $? -ne 0 ]; then
+   exit 1
+fi
 cd -
 
 cd replayer-agent
 go build
+if [ $? -ne 0 ]; then
+   exit 1
+fi
 cd -
